ccdb04fcf9a5a3c2d63ff3d191d02d39f5368312
OoBasili <vasilok11@gmail.com>
2025-06-01 19:21:38 +0300

    Fix tsconfig

--- src/server.ts
+++ src/server.ts
@@ -9,30 +9,11 @@ app.use(express.json());
 
 AppDataSource.initialize()
   .then(() => {
-    console.log('Data Source has been initialized!');
-
-    async function setData() {
-      await AppDataSource.getRepository(User).insert({
-        firstName: 'Vasiliy',
-        lastName: 'Romanov',
-        age: 29,
-        id: crypto.randomUUID(),
-      });
-      app.get('/users', async (req, res) => {
-        const users = await AppDataSource.getRepository(User).find();
-        res.json(users);
-      });
-    }
-    setData();
+    app.get('/users', async (req, res) => {
+      const users = await AppDataSource.getRepository(User).find();
+      res.json(users);
+    });
   })
   .catch((err: Error) => {
     console.error('Error during Data Source initialization:', err);
   });
-
-app.get('/', (req, res) => {
-  res.send('Express + TypeORM + PostgreSQL');
-});
-
-app.listen(port, () => {
-  console.log(`Сервер запущен на порту ${port}`);
-});
